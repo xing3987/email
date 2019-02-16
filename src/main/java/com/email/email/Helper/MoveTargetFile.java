2686e78847a69f29b68ded1f7b127730c26d3613
chenxing <xing3987@163.com>
2019-02-16 17:14:03 +0800

    java file

--- src/main/java/com/email/email/Helper/MoveTargetFile.java
+++ src/main/java/com/email/email/Helper/MoveTargetFile.java
@@ -1,8 +1,10 @@
 package com.email.email.Helper;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 
@@ -10,23 +12,21 @@ public class MoveTargetFile {
     /**
      * 移动指定文件或文件夹(包括所有文件和子文件夹)
      *
-     * @param from
-     *            要移动的文件或文件夹
-     * @param to
-     *            目标文件夹
+     * @param from 要移动的文件或文件夹
+     * @param to   目标文件夹
      * @throws Exception
      */
     public static void MoveFolderAndFileWithSelf(String from, String to) throws Exception {
         try {
             File dir = new File(from);
             // 目标
-            to +=  File.separator + dir.getName();
+            to += File.separator + dir.getName();
             File moveDir = new File(to);
-            if(dir.isDirectory()){
+            if (dir.isDirectory()) {
                 if (!moveDir.exists()) {
                     moveDir.mkdirs();
                 }
-            }else{
+            } else {
                 File tofile = new File(to);
                 dir.renameTo(tofile);
                 return;
@@ -39,7 +39,7 @@ public class MoveTargetFile {
 
             // 文件移动
             for (int i = 0; i < files.length; i++) {
-                System.out.println("文件名："+files[i].getName());
+                System.out.println("文件名：" + files[i].getName());
                 if (files[i].isDirectory()) {
                     MoveFolderAndFileWithSelf(files[i].getPath(), to);
                     // 成功，删除原文件
@@ -51,8 +51,8 @@ public class MoveTargetFile {
                     moveFile.delete();
                 }
                 //筛选需要的文件并移动
-                String filename=files[i].getName();
-                if(filename.endsWith(".mp4") || filename.endsWith(".mkv") || filename.endsWith("wmv")){
+                String filename = files[i].getName();
+                if (filename.endsWith(".mp4") || filename.endsWith(".mkv") || filename.endsWith("wmv")) {
                     files[i].renameTo(moveFile);
                 }
             }
@@ -65,10 +65,8 @@ public class MoveTargetFile {
     /**
      * 移动指定文件或文件夹(包括所有文件和子文件夹)
      *
-     * @param from
-     *            要移动的文件或文件夹
-     * @param to
-     *            目标文件夹
+     * @param from 要移动的文件或文件夹
+     * @param to   目标文件夹
      * @throws Exception
      */
     public static void GetTargetFileAndRename(String from, String to) throws Exception {
@@ -76,11 +74,11 @@ public class MoveTargetFile {
             File dir = new File(from);
             // 目标
             File moveDir = new File(to);
-            if(dir.isDirectory()){
+            if (dir.isDirectory()) {
                 if (!moveDir.exists()) {
                     moveDir.mkdirs();
                 }
-            }else{
+            } else {
                 File tofile = new File(to);
                 dir.renameTo(tofile);
                 return;
@@ -93,20 +91,20 @@ public class MoveTargetFile {
 
             // 文件移动
             for (int i = 0; i < files.length; i++) {
-                System.out.println("文件名："+files[i].getName());
+                System.out.println("文件名：" + files[i].getName());
                 if (files[i].isDirectory()) {
                     GetTargetFileAndRename(files[i].getPath(), to);
                     // 成功，删除原文件
                     files[i].delete();
                 }
                 //筛选需要的文件并移动
-                String filename=files[i].getName();
-                if(filename.endsWith(".mp4") || filename.endsWith(".mkv")
+                String filename = files[i].getName();
+                if (filename.endsWith(".mp4") || filename.endsWith(".mkv")
                         || filename.endsWith(".wmv") || filename.endsWith(".avi")
-                        || filename.endsWith(".wmp")|| filename.endsWith(".rmvb")){
-                    String[] splits=filename.split("\\.");
-                    String endswith=splits[splits.length-1];
-                    File moveFile = new File(moveDir.getPath() + File.separator + new Date().getTime()+"."+endswith);
+                        || filename.endsWith(".wmp") || filename.endsWith(".rmvb")) {
+                    String[] splits = filename.split("\\.");
+                    String endswith = splits[splits.length - 1];
+                    File moveFile = new File(moveDir.getPath() + File.separator + getFileName() + "." + endswith);
                     // 目标文件夹下存在的话，删除
                     if (moveFile.exists()) {
                         moveFile.delete();
@@ -120,9 +118,23 @@ public class MoveTargetFile {
         }
     }
 
+    /**
+     * get file name base on date and timelong
+     *
+     * @return
+     */
+    public static String getFileName() {
+        Date date = new Date();
+        SimpleDateFormat sd = new SimpleDateFormat("yyyyMMdd");
+        String datestr = sd.format(date);
+        String timestr = date.getTime() + "";
+        timestr = timestr.substring(8);
+        return datestr + timestr;
+    }
 
     /**
      * 复制单个文件(可更名复制)
+     *
      * @param oldPathFile 准备复制的文件源
      * @param newPathFile 拷贝到新绝对路径带文件名(注：目录路径需带文件名)
      * @return
@@ -150,8 +162,9 @@ public class MoveTargetFile {
 
     /**
      * 复制单个文件(原名复制)
+     *
      * @param oldPathFile 准备复制的文件源
-     * @param targetPath 拷贝到新绝对路径带文件名(注：目录路径需带文件名)
+     * @param targetPath  拷贝到新绝对路径带文件名(注：目录路径需带文件名)
      * @return
      */
     public static void CopySingleFileTo(String oldPathFile, String targetPath) {
@@ -159,7 +172,7 @@ public class MoveTargetFile {
             int bytesum = 0;
             int byteread = 0;
             File oldfile = new File(oldPathFile);
-            String targetfile = targetPath + File.separator +  oldfile.getName();
+            String targetfile = targetPath + File.separator + oldfile.getName();
             if (oldfile.exists()) { //文件存在时
                 InputStream inStream = new FileInputStream(oldPathFile); //读入原文件
                 FileOutputStream fs = new FileOutputStream(targetfile);
@@ -178,6 +191,7 @@ public class MoveTargetFile {
 
     /**
      * 复制整个文件夹的内容(含自身)
+     *
      * @param oldPath 准备拷贝的目录
      * @param newPath 指定绝对路径的新目录
      * @return
@@ -187,9 +201,9 @@ public class MoveTargetFile {
             new File(newPath).mkdirs(); //如果文件夹不存在 则建立新文件夹
             File dir = new File(oldPath);
             // 目标
-            newPath +=  File.separator + dir.getName();
+            newPath += File.separator + dir.getName();
             File moveDir = new File(newPath);
-            if(dir.isDirectory()){
+            if (dir.isDirectory()) {
                 if (!moveDir.exists()) {
                     moveDir.mkdirs();
                 }
@@ -232,7 +246,7 @@ public class MoveTargetFile {
      */
     public static void main(String[] args) throws Exception {
         // TODO Auto-generated method stub
-        MoveTargetFile databean=new MoveTargetFile();
+        MoveTargetFile databean = new MoveTargetFile();
         databean.GetTargetFileAndRename("G:\\1","G:\\2");
     }
 }
